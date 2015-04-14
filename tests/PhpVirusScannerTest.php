d002c0a1acf314be79b3b54f0ecd4f6a268d8117
MAXakaWIZARD <maxakawizard@gmail.com>
2015-04-14 19:29:36 +0300

    Rewrite tests for using CommandTester

--- tests/PhpVirusScannerTest.php
+++ tests/PhpVirusScannerTest.php
@@ -1,7 +1,9 @@
 <?php
 namespace PhpVirusScanner\Tests;
 
-use Symfony\Component\Process\Process;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
+use PhpVirusScanner\Command\Scan as ScanCommand;
 
 /**
  *
@@ -9,19 +11,45 @@ use Symfony\Component\Process\Process;
 class PhpVirusScannerTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var Application
+     */
+    protected $console;
+
+    /**
+     * @var ScanCommand
+     */
+    protected $command;
+
+    /**
+     * @var CommandTester
+     */
+    protected $commandTester;
+
+    /**
+     *
+     */
+    public function setUp()
+    {
+        $this->console = new Application();
+        $this->console->add(new ScanCommand($this->console));
+
+        $this->command = $this->console->find('scan');
+        $this->commandTester = new CommandTester($this->command);
+    }
+
+    /**
      *
      */
     public function testGeneral()
     {
-        $cmd = "php phpvs scan " . TEST_DATA_PATH . " 'REALLY_BAD_SIGNATURE' --show-full-paths";
-        $process = new Process($cmd, BASE_PATH);
-        $process->run();
+        $this->commandTester->execute([
+            'command' => $this->command->getName(),
+            'dir' => TEST_DATA_PATH,
+            'signature' => 'REALLY_BAD_SIGNATURE',
+            '--show-full-paths' => true
+        ]);
 
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException($process->getErrorOutput());
-        }
-
-        $output = $process->getOutput();
+        $output = $this->commandTester->getDisplay();
 
         $this->assertEquals(true, strpos($output, TEST_DATA_PATH . '/1.php') !== false);
         $this->assertEquals(true, strpos($output, 'Total infected files: 1') !== false);
@@ -32,15 +60,14 @@ class PhpVirusScannerTest extends \PHPUnit_Framework_TestCase
      */
     public function testNothingFound()
     {
-        $cmd = "php phpvs scan " . TEST_DATA_PATH . " 'UNKNOWN_SIGNATURE' --show-full-paths";
-        $process = new Process($cmd, BASE_PATH);
-        $process->run();
+        $this->commandTester->execute([
+            'command' => $this->command->getName(),
+            'dir' => TEST_DATA_PATH,
+            'signature' => 'UNKNOWN_SIGNATURE',
+            '--show-full-paths' => true
+        ]);
 
-        if (!$process->isSuccessful()) {
-            throw new \RuntimeException($process->getErrorOutput());
-        }
-
-        $output = $process->getOutput();
+        $output = $this->commandTester->getDisplay();
 
         $this->assertEquals(true, strpos($output, TEST_DATA_PATH . '/1.php') === false);
         $this->assertEquals(true, strpos($output, 'Nothing found!') !== false);
